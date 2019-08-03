77ac77c3e7ba8986b87498dabd38d7ae2215965f
Florian Dunz <dunz.florian@web.de>
2019-08-03 12:21:47 +0200

    added level goal actor, added 'use' and 'level reset' functionality

---
+++ GMTK2019/Source/GMTK2019/LevelFinishActor.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "Runtime/Engine/Classes/Components/BoxComponent.h"
+#include "LevelFinishActor.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GMTK2019_API ALevelFinishActor : public APaperFlipbookActor {
+	GENERATED_BODY()
+
+		ALevelFinishActor();
+
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION()
+		void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+		void DoActionWhenPlayerOverlaps();
+
+private:
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* BoxComponent;
+
+	bool bIsPlayerOverlapping;
+
+	UPROPERTY(EditAnywhere)
+		FName NextLevelName;
+};
