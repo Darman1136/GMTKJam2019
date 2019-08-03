1bb18561244a8c0e7e5c45e724f5e30b8960fafe
Florian Dunz <dunz.florian@web.de>
2019-08-03 17:31:04 +0200

    added hud, added level end help text

---
+++ GMTK2019/Source/GMTK2019/HUD/GMTK2019HUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "GMTK2019HUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2019_API AGMTK2019HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
