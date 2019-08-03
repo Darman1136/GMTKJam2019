07170c773b9f35b23d5983b3e381579eedb2825c
Florian Dunz <dunz.florian@web.de>
2019-08-03 10:09:11 +0200

    simple playback character which the player can stand on

---
+++ GMTK2019/Source/GMTK2019/Playback/PlaybackCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "PlaybackCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GMTK2019_API APlaybackCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
